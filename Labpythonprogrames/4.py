5e1b27da061009c08baaa572eb42020063fe83bd
Asmitha-Sa <130377662+Asmitha-Sa@users.noreply.github.com>
2023-06-13 15:36:39 +0000

    4

---
+++ Labpythonprogrames/4.py
@@ -0,0 +1,13 @@
+'''Write a python program to convert tuple to a string
+'''
+
+tup=('p', 'y', 't','h','o','n')
+str =  ''.join(tup)
+print(str)
+
+'''Write a python program to reverse a tuple'''
+
+n=input("Enter the string: ")
+x=tuple(n)
+tup=x[::-1]
+print(tup)
